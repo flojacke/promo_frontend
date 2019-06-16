ccdf02d7078a4c89d2b7a1dc444a64117af58c15
flojacke <florian.rey1@gmail.com>
2019-06-16 13:07:55 +0200

    carte fonctionne !!!!

--- src/app/public/home/product-details-modal/product-details-modal.component.ts
+++ src/app/public/home/product-details-modal/product-details-modal.component.ts
@@ -19,24 +19,53 @@ export class ProductDetailsModalComponent implements OnInit {
   yA: number;
   shoplat: number;
   shoplong: number;
+  d: string;
+
   constructor(public bsModalRef: BsModalRef, private promotionService: PromotionControllerService) {}
- 
+
 
   ngOnInit() {
+
 
     this.promotion = new PromotionDTO();
     this.idPromotion = sessionStorage.getItem('idPromotion');
     this.promotionService.connectUsingGET(+(this.idPromotion)).subscribe(
       (promotionRecu: Promotion) => {
         this.promotion = promotionRecu;
-        //this.shoplat = +(this.promotion.shops[0].address.coordinates.latitude);
-        //this.shoplong = +(this.promotion.shops[0].address.coordinates.longitude);
-        this.xA = +(sessionStorage.getItem('geoloclong'));
-        this.yA = +(sessionStorage.getItem('geoloclat'));
-        const pointA = new google.maps.LatLng(48.8782249, 2.3323418);
-        const pointB = new google.maps.LatLng( this.yA, this.xA );
+        this.shoplat = +(this.promotion.shopList[0].address.coordinates.latitude);
+        this.shoplong = +(this.promotion.shopList[0].address.coordinates.longitude);
+        this.initMap( this.shoplong, this.shoplat);
+        this.distance(this.xA, this.yA, this.shoplong, this.shoplat);
+
+      }
+    );
+    this.list.push('PROFIT!!!');
+
+    // this.list.push(this.promotion.product.referenceProduct.name);
+
+  }
+
+  isDataLoaded() {
+    if (this.promotion.id === +(this.idPromotion)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  initMap(longitude: number, latitude: number){
+        if (sessionStorage.getItem('searchlong') == null || sessionStorage.getItem('searchlong') == '0') {
+          this.xA = +(sessionStorage.getItem('geoloclong'));
+          this.yA = +(sessionStorage.getItem('geoloclat'));
+        } else {
+          this.xA = +(sessionStorage.getItem('searchlong'));
+          this.yA = +(sessionStorage.getItem('searchlat'));
+        }
+
+        const pointA =  new google.maps.LatLng( this.yA, this.xA );
+        const pointB = new google.maps.LatLng(latitude, longitude);
         const myOptions = {
-          zoom: 10,
+          zoom: 12,
           center: pointA
         };
         const element = document.getElementById('map');
@@ -56,21 +85,36 @@ export class ProductDetailsModalComponent implements OnInit {
           label: "B",
           map: map
         });
+
+        directionsService.route({
+          origin: pointA,
+          destination: pointB,
+          travelMode: google.maps.TravelMode.WALKING
+          },  function(response, status) {
+          if (status == google.maps.DirectionsStatus.OK) { //console.log('On affiche ou pas !!!')
+            directionsDisplay.setDirections(response);
+          } else {
+          window.alert('Directions request failed due to ' + status);
+          }
+          });
       }
-    );
-    this.list.push('PROFIT!!!');
-  
 
-    // this.list.push(this.promotion.product.referenceProduct.name);
+      distance(xA, yA, xB, yB) {
+        const R = 6371; // km (change this constant to get miles)
+        const dLat = (xB - xA) * Math.PI / 180;
+        const dLon = (yB - yA) * Math.PI / 180;
+        // tslint:disable-next-line:max-line-length
+        const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(xA * Math.PI / 180 ) * Math.cos(xB * Math.PI / 180 ) * Math.sin(dLon/2) * Math.sin(dLon/2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        let d = R * c;
+        if (d > 1) {
+          d = Math.round(d * 100) / 100;
+          this.d = d + 'km';
+        } else {
+            d = Math.round(d * 1000);
+            this.d = d + 'm';
+        }
+      }
 
   }
 
-  isDataLoaded() {
-    if (this.promotion.id === +(this.idPromotion)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-}
