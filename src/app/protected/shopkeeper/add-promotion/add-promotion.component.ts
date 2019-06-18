7491b0a79329f1b57332b746007ee4ec41643735
Nicolas Gondran <nicolas.gondran@gmail.com>
2019-06-18 19:46:36 +0200

    Sangoku le plus fort des sayian : addPromotion form update

--- src/app/protected/shopkeeper/add-promotion/add-promotion.component.ts
+++ src/app/protected/shopkeeper/add-promotion/add-promotion.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AddPromotionControllerService } from 'src/app/shared/api/api.';
+import { ReferenceProduct } from 'src/app/shared/models/models';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-promotion',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPromotionComponent implements OnInit {
 
-  constructor() { }
+  products: ReferenceProduct [];
+  productSubscription: Subscription;
+  constructor(private addPromotionControllerService: AddPromotionControllerService) { }
 
   ngOnInit() {
+    this.getAllProductList();
+  }
+
+  getShopList() {
+    let userId: number;
+
+    console.log('getShopList button works');
+    // recuperer l'id utilisateur et le + signifie parse du stringtonumber
+    userId = (+(sessionStorage.getItem('userConnecte')));
+    // recuperer la list des shops problème pour la récuperer
+
+  }
+
+  getAllProductList() {
+    this.productSubscription = this.addPromotionControllerService.referenceProductListUsingGET().subscribe(data => {this.products = data; });
+
+  }
+
+  onSubmit() {
+
   }
 
 }
